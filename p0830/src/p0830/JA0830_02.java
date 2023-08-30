97b63615392a4a525899d17bf3acbb5d8e2b0c08
KOREAVC <KOREAVC@DESKTOP-JJRI82U>
2023-08-30 18:12:21 +0900

    8월 30일 수업 - 변수

---
+++ p0830/src/p0830/JA0830_02.java
@@ -0,0 +1,26 @@
+package p0830;
+
+public class JA0830_02 {
+	public static void main(String[] args) {
+		boolean bo = true;
+		boolean bo2 = false;
+		char ch = 'a';
+		char ch2 = 'b';
+		byte by = 1;
+		short sh = 2;
+		int a = 1;
+		int b = 20;
+		int in = 2100000000;
+		long lo = 100L;
+		float fl = 1.2f;
+		double du = 1.345234;
+		double d = 11.12;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+	}
+}
---
+++ p0830/src/p0830/JA0830_03.java
@@ -0,0 +1,23 @@
+package p0830;
+
+public class JA0830_03 {
+
+	public static void main(String[] args) {
+		final int NUM3 =100; //final int 상수 이므로 num3 =1 이 되지 않는다, 대문자로 표시해줌
+		int num1 = 100;   //변수 선언
+		int num2 = 200;
+		int temp; // System.out.println(temp); 값이 들어 가있지 않아 에러가 난다, 변수를 선언한후 값이 들어가야 함
+		int company_intro;
+		int companyIntro;
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+		System.out.println(num1%num2);
+		
+		num1=10;
+		num1=300;
+
+	}
+
+}
